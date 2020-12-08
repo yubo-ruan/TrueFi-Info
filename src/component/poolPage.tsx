74ffc38569bf6c3e3bfa63df38849c85470096b8
Yubo R <yubor@DNa80947a.SUNet>
2020-12-07 20:45:27 -0800

    add recharts and pool composition

--- src/component/poolPage.tsx
+++ src/component/poolPage.tsx
@@ -1,73 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { Area } from '@ant-design/charts';
+import React, { useState, useEffect } from 'react'
 import {Typography, Statistic, Card, Row, Col, Divider, Table} from 'antd'
-import {getTfiTotalSupply, getPoolValue, getPoolJoined, getPoolExited, getNetPool, getNetCurve, getBorrow} from '../hooks/pool'
-const { Title, Paragraph, Text, Link } = Typography;
+import {getTfiTotalSupply, getPoolValue, getPoolChart, getNetCurve, getLoanChart, TusdHistoricalBal} from '../hooks/pool'
+import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts'
+
+const { Title, Paragraph, Text, Link } = Typography
 
 
 export const PoolPage: React.FC = () => {
   
   const [tfi, setTfi] = useState({supply: 0, poolValue: 0})
-  const [joined, setjoined] = useState([{total:0, value:0, blockNumber:0}])
-  const [exited, setExited] = useState([{total:0, value:0, blockNumber:0}])
-  const [poolValue, setPoolValue] = useState([{total:0, value:0, blockNumber:0}])
-  const [curve, setCurve] = useState([{total:0, value:0, blockNumber:0}])
+  const [poolChart, setPoolChart] = useState([{total:0, value:0, blockNumber:0}])
+  const [curveChart, setCurveChart] = useState([{total:0, value:0, blockNumber:0}])
+  const [loanChart, setLoanChart] = useState([{total:0, value:0, blockNumber:0}])
+  const [combinedChart, setCombinedChart] = useState([{TUSD:0, yCRV:0, blockNumber:0}])
 
   useEffect(() => {
-    getPoolJoined().then(res => setjoined(res))
-    getPoolExited().then(res => setExited(res))
     getTfiTotalSupply().then(res => setTfi(prev => {
       return {...prev, supply: res}
     }))
     getPoolValue().then(res => setTfi(prev => {
       return {...prev, poolValue: res}
     }))
-    getNetPool().then(res => setPoolValue(res))
-    getNetCurve().then(res => setCurve(res))
-    getBorrow()
+    getPoolChart().then(res => setPoolChart(res))
+    getNetCurve().then(res => setCurveChart(res))
+    getLoanChart().then(res => setLoanChart(res))
+    TusdHistoricalBal().then(res => setCombinedChart(res))
   }, []);
 
-  
-  const curveConfig = {
-    data: curve,
-    padding: 80,
-    xField: 'blockNumber',
-    yField: 'total',
-    xAxis: { tickCount: 5 },
-    areaStyle: function areaStyle() {
-      return { fill: 'l(270) 0:#ffffff 0.5:#7ec2f3 1:#1890ff' };
-    },
-  };
-  const netValueConfig = {
-    data: poolValue,
-    padding: 80,
-    xField: 'blockNumber',
-    yField: 'total',
-    xAxis: { tickCount: 5 },
-    areaStyle: function areaStyle() {
-      return { fill: 'l(270) 0:#ffffff 0.5:#7ec2f3 1:#1890ff' };
-    },
-  };
-  const joinedConfig = {
-    data: joined,
-    padding: 80,
-    xField: 'blockNumber',
-    yField: 'total',
-    xAxis: { tickCount: 5 },
-    areaStyle: function areaStyle() {
-      return { fill: 'l(270) 0:#ffffff 0.5:#7ec2f3 1:#1890ff' };
-    },
-  };
-  const exitedConfig = {
-    data: exited,
-    padding: 80,
-    xField: 'blockNumber',
-    yField: 'total',
-    xAxis: { tickCount: 5 },
-    areaStyle: function areaStyle() {
-      return { fill: 'l(270) 0:#ffffff 0.5:#7ec2f3 1:#1890ff' };
-    },
-  };
   return(
     <div>
       <Row gutter={16}>
@@ -82,17 +41,42 @@ export const PoolPage: React.FC = () => {
           </Card>
         </Col>
       </Row>  
-      <Divider />
-        <Title level={4}>Pool Value (TUSD)</Title>
-        <Area {...netValueConfig} />
-
-        <Title level={4}>TUSD to Curve (TUSD)</Title>
-        <Area {...curveConfig} />
-        <Title level={4}>Capital Joined (TUSD)</Title>
-        <Area {...joinedConfig} />
-        <Title level={4}>Capital Exited (TUSD)</Title>
-        <Area {...exitedConfig} />
-      <Divider />
+      <LineChart width={1000} height={300} data={poolChart} margin={{top: 30, right: 30, left: 30, bottom: 30,}}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="blockNumber" />
+        <YAxis type="number" tickMargin={10} />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="total" stroke="#8884d8" />
+        <Line type="monotone" dataKey="value" stroke="#82ca9d" />
+      </LineChart>
+      <LineChart width={1000} height={300} data={curveChart} margin={{top: 30, right: 30, left: 30, bottom: 30,}}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="blockNumber" />
+        <YAxis type="number" tickMargin={10} />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="total" stroke="#8884d8" />
+        <Line type="monotone" dataKey="value" stroke="#82ca9d" />
+      </LineChart>
+      <LineChart width={1400} height={500} data={loanChart} margin={{top: 30, right: 30, left: 30, bottom: 30,}}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="blockNumber" />
+        <YAxis type="number" tickMargin={10} />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="total" stroke="#8884d8" />
+        <Line type="monotone" dataKey="value" stroke="#82ca9d" />
+      </LineChart>
+      <LineChart width={1400} height={500} data={combinedChart} margin={{top: 30, right: 30, left: 30, bottom: 30,}}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="blockNumber" />
+        <YAxis type="number" tickMargin={10} />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="TUSD" stroke="#8884d8" />
+        <Line type="monotone" dataKey="yCRV" stroke="#82ca9d" />
+      </LineChart>
     </div>
   )
 };
--- src/hooks/pool.ts
+++ src/hooks/pool.ts
@@ -1,11 +1,15 @@
-import React from 'react';
-import { ethers, Contract } from 'ethers'
+import { ethers } from 'ethers'
 import { connect } from './providers'
 import { contracts } from './constants'
+import { stringify } from 'querystring'
 
 const [network, provider, wallet] = connect()
 const abi = ['function totalSupply() public view returns (uint256)','function poolValue() public view returns (uint256)']
+const tusdAbi = ["event Transfer(address indexed src, address indexed dst, uint val)"]
+const curveGaugeAbi = ['event Deposit(address indexed provider, uint256 value)','event Withdraw(address indexed provider, uint256 value)']
 const tfi = new ethers.Contract(contracts.tfi, abi, wallet) 
+const tusd = new ethers.Contract(contracts.tusd, tusdAbi, wallet) 
+const curveGauge = new ethers.Contract(contracts.curveGauge, curveGaugeAbi, wallet) 
 
 export const getTfiTotalSupply = async () => {
     return await tfi.totalSupply()/1e18
@@ -16,16 +20,20 @@ export const getPoolValue = async () => {
 }
 
 
-const getEventsHelper = async (topic:string,startIndex:number,endIndex:number) => {
+const getEventsHelper = async (topic:string,index:number) => {
     let result = []
     let total = 0
     const logInfo = {address: contracts.tfi, topics:[ethers.utils.id(topic)], fromBlock: 0, toBlock: "latest"}    
     const res = await provider.getLogs(logInfo)
+    
     for(let i=0;i<res.length;i++){
-        const value = parseInt(res[i]['data'].substr(startIndex,endIndex),16)/1e18
-        if(topic == 'Borrow(address,uint256,uint256)'){
-            console.log(res[i])
-            console.log(res[i]['data'].substr(startIndex,endIndex))
+        const value = parseInt(res[i]['data'].substr(2+64*index,64),16)/1e18
+        if(topic === '1Joined(address,uint256,uint256)'){
+            console.log('default value: ' + value)
+            console.log('1:' + parseInt(res[i]['data'].substr(2,64),16)/1e18)
+            console.log('2:' + parseInt(res[i]['data'].substr(66,64),16)/1e18)
+            console.log('3:' + parseInt(res[i]['data'].substr(129,64),16)/1e18)
+            console.log('topics:'+res[i].topics)
         }
         total += value
         result.push({total: total,
@@ -35,35 +43,63 @@ const getEventsHelper = async (topic:string,startIndex:number,endIndex:number) =
     return result
 }
 const mergeArray = (array: any[]) => {
+    
     array.sort((a,b) => (a.blockNumber > b.blockNumber) ? 1 : ((b.blockNumber > a.blockNumber) ? -1 : 0)); 
     for(let i=1;i<array.length;i++){
         array[i].total = array[i-1].total + array[i].value
-        if(array[i].blockNumber == array[i-1].blockNumber){
-        array.splice(i-1,1)
-        i--
+        if(array[i].blockNumber === array[i-1].blockNumber){
+            array.splice(i-1,1)
+            i--
         }
     }
     return array
 }
 
 export const getPoolJoined = async () => {
-    return getEventsHelper('Joined(address,uint256,uint256)',0,66)
+    return getEventsHelper('Joined(address,uint256,uint256)',0)
 }
 export const getPoolExited = async () => {
-    return getEventsHelper('Exited(address,uint256)',0,66)
+    const array = await getEventsHelper('Exited(address,uint256)',0)
+    array.forEach(element => {
+      element.value *= -1                       //turn into nagative value
+    })
+    return array
 }
-export const getNetPool = async () => {
-    const array = [...await getPoolJoined(),...await getPoolExited()]
-    return mergeArray(array)
-}
-
 
 export const getFlushed = async () => {
-    return getEventsHelper('Flushed(uint256)',34,65)
+    return getEventsHelper('Flushed(uint256)',0)
 }
 
 export const getPulled = async () => {
-    return getEventsHelper('Pulled(uint256)',34,65)
+    const array = await getEventsHelper('Pulled(uint256)',0)
+    array.forEach(element => {
+        element.value *= -1                       //turn into nagative value
+      })
+    return array
+}
+
+export const getBorrow = async () => {
+    return getEventsHelper('Borrow(address,uint256,uint256)',1)
+}
+
+export const getRepaid = async () => {
+    const array = await getEventsHelper('Repaid(uint256)',0)
+    array.forEach(element => {
+        element.value *= -1                       //turn into nagative value
+      })
+    return array
+}
+
+export const getCollected = async () => {
+    return getEventsHelper('Collected(uint256)',0)
+}
+
+
+
+
+export const getPoolChart = async () => {
+    const array = [...await getPoolJoined(),...await getPoolExited()]
+    return mergeArray(array)
 }
 
 export const getNetCurve = async () => {
@@ -71,10 +107,80 @@ export const getNetCurve = async () => {
     return mergeArray(array)
 }
 
-export const getBorrow = async () => {
-    return getEventsHelper('Borrow(address,uint256,uint256)',66,67)
+export const getLoanChart = async () => {
+    const array = [...await getBorrow(),...await getRepaid()]
+    return mergeArray(array)
 }
 
-export const getRepaid = async () => {
-    return getEventsHelper('Repaid(uint256)',34,65)
+export const getCombined = async () => {
+    const pool = await getPoolChart()
+    const curve = await getNetCurve()
+    return [...pool,...curve]
+
+}
+
+
+const processArray = (array: any[], name: string) => {
+    let newArray = []
+    for(let i=1;i<array.length;i++){
+        newArray.push({data:{name:name,balance:array[i].total},blockNumber:array[i].blockNumber})
+    }
+    return newArray
+}
+
+const mergeArrayNew = (array: any[]) => {
+    let newArray:any = []
+    array.sort((a,b) => (a.blockNumber > b.blockNumber) ? 1 : ((b.blockNumber > a.blockNumber) ? -1 : 0)); 
+    if(array[0].data.name === 'TUSD'){
+        newArray[0] = {TUSD:array[0].data.balance,yCRV:0,blockNumber:array[0].blockNumber}
+    }else{
+        newArray[0] = {TUSD:0,yCRV:array[0].data.balance,blockNumber:array[0].blockNumber}
+    }
+    
+    for(let i=1;i<array.length;i++){
+        if(array[i].data.name === 'TUSD'){
+            newArray.push({TUSD:array[i].data.balance,yCRV: newArray[i-1].yCRV,blockNumber:array[i].blockNumber})
+        }
+        if(array[i].data.name === 'yCRV'){
+            newArray.push({TUSD:newArray[i-1].TUSD,yCRV:array[i].data.balance,blockNumber:array[i].blockNumber})
+        }
+    }
+    
+    return newArray
+}
+
+export const TusdHistoricalBal = async () => {
+
+    
+    const tusdOutFilter = {address: contracts.tusd, topics:tusd.filters.Transfer(contracts.tfi).topics, fromBlock: 0, toBlock: "latest"}    
+    const tusdInFilter = {address: contracts.tusd, topics:tusd.filters.Transfer(null,contracts.tfi).topics, fromBlock: 0, toBlock: "latest"}    
+
+    const curveOutFilter = {address: contracts.curveGauge, topics:curveGauge.filters.Withdraw(contracts.tfi).topics, fromBlock: 0, toBlock: "latest"}    
+    const curveInFilter = {address: contracts.curveGauge, topics:curveGauge.filters.Deposit(contracts.tfi).topics, fromBlock: 0, toBlock: "latest"}
+
+    const tusdArray = mergeArray([...await eventHelper(tusdOutFilter,-1),...await eventHelper(tusdInFilter,1)])
+    const curveArray = mergeArray([...await eventHelper(curveOutFilter,-1),...await eventHelper(curveInFilter,1)])
+
+
+    
+    const combined = mergeArrayNew([...processArray(tusdArray,'TUSD'),...processArray(curveArray,'yCRV')])
+
+
+    return combined
+}
+
+const eventHelper = async (filter: ethers.providers.Filter, sign:number) => {
+    let result: { total: number; value: number; blockNumber: number }[] = []
+    let total = 0 
+    const res = await provider.getLogs(filter).then(res => {
+        for(let i=0;i<res.length;i++){
+            const value = parseInt(res[i]['data'].substr(2,64),16)/1e18
+            
+            result.push({total: 0,
+                        value: value*sign,
+                        blockNumber: res[i]['blockNumber']})
+        }
+    })
+    
+    return result
 }
